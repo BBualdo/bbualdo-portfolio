c299890a259a3372c45d24aa3085e668bee98311
Sebastian <mrseb10@gmail.com>
2023-10-16 20:31:52 +0200

    Added Home Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,34 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
-      primary: 'Orbitron',
-      secondary: 'Rajdhani',
-      tertiary: 'Aldrich',
+      primary: "Orbitron",
+      secondary: "Rajdhani",
+      tertiary: "Aldrich",
     },
     container: {
       padding: {
-        DEFAULT: '15px',
+        DEFAULT: "15px",
       },
     },
     screens: {
-      xs: '360px',
-      sm: '640px',
-      md: '768px',
-      lg: '960px',
-      xl: '1200px',
+      xs: "360px",
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
     },
     extend: {
       colors: {
-        primary: '#0a0a0a',
-        accent: '#B809C3',
-        aqua: '#0bb38b',
+        primary: "#0a0a0a",
+        accent: "#B809C3",
+        aqua: "#0bb38b",
       },
       backgroundImage: {
         site: "url('./assets/site-bg.jpg')",
         about: "url('./assets/about.png')",
+        hacking: "url('./assets/hacking.webp')",
+        music: "url('./assets/music.jpg')",
       },
     },
   },
