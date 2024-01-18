abef67bb8cd3d31f879272924db7cf3dbc28f961
Sebastian <mrseb10@gmail.com>
2024-01-18 17:30:24 +0100

    Update Portfolio

--- src/data/data.js
+++ src/data/data.js
@@ -1,30 +1,30 @@
 export const programmingData = [
   {
     key: 0,
+    href: "https://life-is-a-game.vercel.app",
+    imageUrl: "/assets/liag.png",
+    technologies: "NextJS/TypeScript/TailwindCSS",
+    name: "Life is a Game",
+  },
+  {
+    key: 1,
     href: "https://classcarsllc.com",
     imageUrl: "/assets/classcars.png",
     technologies: "NextJS/TypeScript/TailwindCSS",
     name: "ClassCarsLLC Website",
   },
   {
-    key: 1,
+    key: 2,
     href: "https://bbualdo-kanban.vercel.app",
     imageUrl: "/assets/kanban.png",
     technologies: "NextJS/TypeScript/Redux Toolkit/TailwindCSS",
     name: "Kanban",
   },
   {
-    key: 2,
+    key: 3,
     href: "https://bbualdo-audiophile.netlify.app",
     imageUrl: "/assets/audiophile.png",
     technologies: "React/JSON/TailwindCSS",
     name: "Audiophile e-commerce",
-  },
-  {
-    key: 3,
-    href: "https://bbualdo-pomodoro.vercel.app/",
-    imageUrl: "/assets/pomodoro.png",
-    technologies: "React/TypeScript/Sass",
-    name: "Pomodoro App",
   },
 ];
